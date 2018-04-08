13ddc1602a48164e82a1579bcc974c4ac678dbbd
Subham Rakshit <subham.rakshit@hpe.com>
2018-04-08 23:51:25 +0100

    Initial commit

---
+++ src/api/controllers/registration.js
@@ -0,0 +1,39 @@
+"use strict"
+
+var errs = require("restify-errors");
+var validator = require("validator");
+
+module.exports = {
+    createNeighbour: createNeighbour
+};
+
+function createNeighbour(req, res, next) {
+    let neighbourDetails = req.swagger.params["NeighbourDetails"].value;
+
+    //Validate all fields have non-empty strings
+    for(var key in neighbourDetails) {
+        if (neighbourDetails[key] == "") {
+            return next(new errs.InvalidContentError(key + " is empty!"));
+        } else {
+            switch(key) {
+                case "email" :
+                    if (!validator.isEmail(neighbourDetails[key])) {
+                        return next(new errs.InvalidContentError(key + " is invalid!"));
+                    }
+                    break;
+                case "phone" :
+                    if (!validator.isMobilePhone(neighbourDetails[key], "en-GB")) {
+                        return next(new errs.InvalidContentError(key + " is invalid!"));
+                    }
+                    break;
+                case "postcode" :
+                    if (!validator.isPostalCode(neighbourDetails[key], "GB")) {
+                        return next(new errs.InvalidContentError(key + " is invalid!"));
+                    }
+            }
+        }
+    }
+    
+    res.send(201, {message: "Neighbour created!"});
+    return next();
+}
