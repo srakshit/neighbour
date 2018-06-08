cd455a9baae3503ebe5a1980406a918f546de039
Subham Rakshit <developersubham@gmail.com>
2018-06-08 17:27:10 +0100

    Moved code to allocation.js

--- api/controllers/registration.js
+++ api/controllers/registration.js
@@ -110,24 +110,6 @@ function getSubscriberbyId(req, res, next) {
     }
 }
 
-function getCatchersAllocatedToSubscriber(req, res, next) {
-    let uid = req.swagger.params.uid.value;
-    
-    subscribers.getCatchersAllocatedToSubscriber(uid)
-            .then((allocatedCatcher) => {
-                if (allocatedCatcher) {
-                    res.send(200, allocatedCatcher);
-                    return next();
-                }else {
-                    return next(new errs.ResourceNotFoundError('No catcher is allocated to the subscriber!'))
-                }
-            })
-            .catch((err) => {
-                //TODO: Test code path
-                return next(new errs.InternalError(err.message, 'Failed to retrieve catcher allocation to subscriber'));
-            });
-}
-
 function getSubscriberByEmail(req, res, next) {
     let email = req.swagger.params.email.value;
     
@@ -146,82 +128,9 @@ function getSubscriberByEmail(req, res, next) {
         });
 }
 
-function allocateCatcherToSubscriber(req, res, next) {
-    let subscriber_uid = req.swagger.params.uid.value;
-    let catcher_ref = req.swagger.params.catcherRef.value;
-    
-    subscribers.getByUid(subscriber_uid)
-        .then((subscriber) => {
-            if (subscriber) {
-                subscribers.getCatcherById(catcher_ref)
-                .then((catcher) => {
-                    if (catcher) {
-                        subscribers.allocateCatcher(catcher.id, subscriber.id)
-                            .then(() => {
-                                res.send(201, {
-                                    message: 'Subscriber ' + subscriber.firstName + ' ' + subscriber.lastName + ' is allocated to catcher ' + catcher.firstName + ' ' + catcher.lastName, 
-                                    catcher_ref: catcher.catcher_id, 
-                                    subscriber_ref: subscriber.subscriber_id
-                                });
-                                return next();
-                            })
-                            .catch((err) => {
-                                let errMsg = err.message.toLowerCase();
-                                if (new RegExp(/unique constraint/).test(errMsg)) {
-                                    return next(new errs.ConflictError('Catcher is already allocated to subscriber!'));
-                                }
-                                //TODO: Test code path
-                                return next(new errs.InternalError(err.message, 'Failed to allocate catcher to subscriber!'));
-                            });
-                    }else {
-                        return next(new errs.ResourceNotFoundError('Catcher with ref ' + catcher_ref + 'is not found!'));
-                    }
-                });
-            }else {
-                return next(new errs.ResourceNotFoundError('Subscriber with uid ' + subscriber_uid + 'is not found!'));
-            }
-        });
-}
-
-function updateCatcherAllocationForSubscriber(req, res, next) {
-    let subscriber_uid = req.swagger.params.uid.value;
-    let catcher_ref = req.swagger.params.catcherRef.value;
-    
-    subscribers.getByUid(subscriber_uid)
-        .then((subscriber) => {
-            if (subscriber) {
-                subscribers.getCatcherById(catcher_ref)
-                .then((catcher) => {
-                    if (catcher) {
-                        subscribers.updateCatcherAllocation(catcher.id, subscriber.id)
-                            .then(() => {
-                                res.send(200, {
-                                    message: 'Subscriber ' + subscriber.firstName + ' ' + subscriber.lastName + ' is now allocated to catcher ' + catcher.firstName + ' ' + catcher.lastName, 
-                                    catcher_ref: catcher.catcher_id, 
-                                    subscriber_ref: subscriber.subscriber_id
-                                });
-                                return next();
-                            })
-                            .catch((err) => {
-                                return next(new errs.InternalError(err.message, 'Failed to allocate catcher to subscriber!'));
-                            });
-                    }else {
-                        return next(new errs.ResourceNotFoundError('Catcher with ref ' + catcher_ref + 'is not found!'));
-                    }
-                });
-            }else {
-                return next(new errs.ResourceNotFoundError('Subscriber with uid ' + subscriber_uid + 'is not found!'));
-            }
-        });
-}
-
-
 module.exports = {
     addSubscriber: addSubscriber,
     updateSubscriber: updateSubscriber,
     getSubscriberbyId: getSubscriberbyId,
-    getSubscriberByEmail: getSubscriberByEmail,
-    getCatchersAllocatedToSubscriber: getCatchersAllocatedToSubscriber,
-    allocateCatcherToSubscriber: allocateCatcherToSubscriber,
-    updateCatcherAllocationForSubscriber: updateCatcherAllocationForSubscriber
+    getSubscriberByEmail: getSubscriberByEmail
 };
